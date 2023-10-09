643449a59a4c1754081cf1ffb70c81a135b1eb44
Akshay-Gengaje <akshaysgengaje@gmail.com>
2023-10-09 20:36:59 +0530

    Project - UsePopCorn - Components splitted

--- src/Components/NavBar/Logo.js
+++ src/Components/NavBar/Logo.js
@@ -1,7 +1,10 @@
-import React from "react";
-
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
 }
 
 export default Logo;
